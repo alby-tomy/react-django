814f074671a998c78c0a2fc49218de6be74cd1a5
alby-tomy <alby.u.tomy@gmail.com>
2024-05-28 07:06:26 +0530

    api env ProtectedRoute

---
+++ fontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import {Navigate} from "react-router-dom"
+import {jwtDecode} from "jwt-decode"
+import api from "../api"
+import { REFRESH_TOKEN,ACCESS_TOKEN } from "../constants"
+
+
+function ProtectedRoute({children}){
+    const [isAuthorized, setIsAuthorized] = useState(null)
+
+    const refreshToken = async() => {
+
+    }
+    const auth = async() =>{
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if(!token){
+            setIsAuthorized(false)
+            return
+        }
+        const decode = jwtDecode(token)
+        const tokenExpiration = decode.exp
+        const now = Date.now() / 1000
+        
+        if (tokenExpiration < now){
+            await refreshToken()
+        }else{
+            setIsAuthorized(true)
+        }
+    }
+
+    if (isAuthorized === null){
+        return <div>Loading...</div>
+    }
+
+    return isAuthorized? children : <Navigate to = "/login" />
+
+}
+
+
+export default ProtectedRoute
